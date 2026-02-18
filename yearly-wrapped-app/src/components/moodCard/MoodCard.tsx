f442af9726ad49b3864b24f4b3e7c9a8096d8baf
Sam Akiode <sam.akiode03@gmail.com>
2026-02-18 17:22:09 +0000

    made small design changes to analytics section on mobile

--- yearly-wrapped-app/src/components/moodCard/MoodCard.tsx
+++ yearly-wrapped-app/src/components/moodCard/MoodCard.tsx
@@ -14,9 +14,9 @@ const MoodCard: React.FC<MoodCardProps> = ({ mood, dayCount, totalDays }) => {
   const percentage: number = (dayCount / totalDays) * 100;
   return (
     <>
-      <div className="mood-card color-primary background-tertiary prevent-select p-3 d-flex flex-column justify-content-start gap-3">
+      <div className="mood-card color-primary background-tertiary prevent-select p-3 d-flex flex-column justify-content-start gap-2">
         <div className={`mood-card-icon background-${mood.label.toLowerCase()}`}></div>
-        <div className="mood-card-content">
+        <div className="mood-card-content mt-1">
           <h5 className="mood-card-percentage text-center m-0">{percentage.toFixed(0)}%</h5>
           <p className="mood-card-count text-center m-0">{dayCount} of {totalDays} days</p>
         </div>

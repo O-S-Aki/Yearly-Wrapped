c750476e12c2df2835fdde8a1645511b35b83767
Sam Akiode <sam.akiode03@gmail.com>
2026-02-19 13:22:10 +0000

    small changes to carousel navigation controls

--- yearly-wrapped-app/src/components/carousel/Carousel.tsx
+++ yearly-wrapped-app/src/components/carousel/Carousel.tsx
@@ -88,24 +88,24 @@ const Carousel: React.FC<CarouselProps> = ({children, onIndexChange, initialInde
           <>
             <div className="navigation-controls d-flex flex-row justify-content-center align-items-center px-2">
               <div className="navigation-buttons d-flex flex-row gap-3">
-                <button className="btn embla__prev background-background color-primary" onClick={navigateToPrevious}>
+                <div className="btn embla__prev background-background color-primary" onClick={navigateToPrevious}>
                   <i className="bi bi-chevron-left "></i>
-                </button>
-                <button className="btn embla__next background-background color-primary" onClick={navigateToNext}>
+                </div>
+                <div className="btn embla__next background-background color-primary" onClick={navigateToNext}>
                   <i className="bi bi-chevron-right"></i>
-                </button>
+                </div>
               </div>
             </div>
           </>) : (
           <>
             <div className="navigation-controls d-flex flex-row justify-content-between align-items-center px-2">
               <div className="navigation-buttons d-flex flex-row gap-3">
-                <button className="btn embla__prev background-background color-primary" onClick={navigateToPrevious}>
+                <div className="btn embla__prev background-background color-primary" onClick={navigateToPrevious}>
                   <i className="bi bi-chevron-left "></i>
-                </button>
-                <button className="btn embla__next background-background color-primary" onClick={navigateToNext}>
+                </div>
+                <div className="btn embla__next background-background color-primary" onClick={navigateToNext}>
                   <i className="bi bi-chevron-right"></i>
-                </button>
+                </div>
               </div>
               <div className="line prevent-select">.</div>
               <div className="navigation-dots d-flex flex-row align-items-center justify-content-end">
@@ -113,8 +113,8 @@ const Carousel: React.FC<CarouselProps> = ({children, onIndexChange, initialInde
                   <div className="button-dots-container d-flex flex-row gap-2">
                     {
                       scrollSnaps.map((_, index) => (
-                        <button className={`embla__dot ${index == selectedSnap ? 'embla__dot--selected' : ''} background-background`} key={index}
-                          onClick={() => navigateTo(index)}></button>
+                        <div className={`embla__dot ${index == selectedSnap ? 'embla__dot--selected' : ''} background-background`} key={index}
+                          onClick={() => navigateTo(index)}></div>
                       ))
                     }
                   </div>
